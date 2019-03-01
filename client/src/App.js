2ce0471fa7b4225d1f96ac10f5e6fc5c502b826f
Elaine <elaine.cadman@gmail.com>
2019-02-28 23:02:31 -0800

    cleaner file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
+import AllWines from "./pages/allwines";
+import Wrapper from "./components/Wrapper";
+import Footer from "../src/components/Footer";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+function App() {
+  return (
+    <Router>
+      <div>
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/allwines" component={AllWines} />
+          {/* <Route exact path="/about" component={About} /> */}
+        </Wrapper>
+        <Footer />
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
