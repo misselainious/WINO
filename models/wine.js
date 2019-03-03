6c714e283c044da64b45e9ae2944b93f4cb7da99
FyperTheViper <b.w.byars@gmail.com>
2019-03-03 13:30:25 -0800

    added michael's wine.js changes

--- models/wine.js
+++ models/wine.js
@@ -1,7 +1,6 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
 
-var wineSchema = new Schema({
+const wineSchema = new mongoose.Schema({
 
         code: String,
         country: String,
@@ -19,7 +18,7 @@ var wineSchema = new Schema({
         disgorgement_date: String,
         closure: String,
         annual_cases_produced: Number,
-        varieties: String,
+        constieties: String,
         maceration_notes: String,
         pumpover: String,
         destemming: String,
@@ -45,4 +44,11 @@ var wineSchema = new Schema({
         notes: String
     });
 
-module.exports = mongoose.model('Wine', wineSchema);
+const wine = mongoose.model('wineList', wineSchema, "wineList");
+
+wine.findOne((err, result) => {
+    if (err) { return err }
+    console.log("wine find results", result)
+})
+
+module.exports = wine
