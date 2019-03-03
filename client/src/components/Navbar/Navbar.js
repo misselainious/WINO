68cc736b85f009c018a50154a47912d409d73913
Susye Weng-Reeder <weng-reeder@hotmail.com>
2019-03-02 23:52:02 -0800

    homepage up with all routes connected

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Homepage from '../Homepage/Homepage';
 import {
   Button,
   Container,
   // Divider,
-  Grid,
-  Header,
+  // Grid,
+  // Header,
   Icon,
   // Image,
   // List,
@@ -17,58 +18,13 @@ import {
 } from 'semantic-ui-react'
 
 
-// Heads up!
-// We using React Static to prerender our docs with server side rendering, this is a quite simple solution.
-// For more advanced usage please check Responsive docs under the "Usage" section.
 const getWidth = () => {
   const isSSR = typeof window === 'undefined'
 
   return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
 }
 
-// const Navbar = ({ mobile }) => (
-/* eslint-disable react/no-multi-comp */
-/* Heads up! navbar uses inline styling, however it's not the best practice. Use CSS or styled components for
- * such things.
- */
-// const Home = ({ mobile }) => (
-//   <Container text>
-//     <Header
-//       as='h1'
-//       content='Wine Wise'
-//       inverted
-//       style={{
-//         fontSize: mobile ? '2em' : '4em',
-//         fontWeight: 'normal',
-//         marginBottom: 0,
-//         marginTop: mobile ? '1.5em' : '3em',
-//       }}
-//     />
-//     <Header
-//       as='h2'
-//       content='Do whatever you want when you want to.'
-//       inverted
-//       style={{
-//         fontSize: mobile ? '1.5em' : '1.7em',
-//         fontWeight: 'normal',
-//         marginTop: mobile ? '0.5em' : '1.5em',
-//       }}
-//     />
-//     <Button primary size='huge'>
-//       Get Started
-//       <Icon name='right arrow' />
-//     </Button>
-//   </Container>
-// )
 
-// Home.propTypes = {
-//   mobile: PropTypes.bool,
-// }
-
-/* Heads up!
- * Neither Semantic UI nor Semantic UI React offer a responsive Navbar, however, it can be implemented easily.
- * It can be more complicated, but you can create really flexible markup.
- */
 class Navbar extends Component {
   state = {}
 
@@ -80,7 +36,7 @@ class Navbar extends Component {
     const { fixed } = this.state
 
     return (
-      <Responsive className="backgroundImage" getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
+      <Responsive className="backgroundImage" src="winePouring.jpg" getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
         <Visibility
           once={false}
           onBottomPassed={this.showFixedMenu}
@@ -108,7 +64,7 @@ class Navbar extends Component {
                 <Menu.Item as='a'>About Us</Menu.Item>
                 <Menu.Item position='right'>
                   <Button as='a' inverted={!fixed}>
-                    Log in
+                    Register
                   </Button>
                   <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
                     Members
@@ -116,7 +72,7 @@ class Navbar extends Component {
                 </Menu.Item>
               </Container>
             </Menu>
-            {/* <Navbar /> */}
+            {/* <Homepage /> */}
           </Segment>
         </Visibility>
 
@@ -162,7 +118,7 @@ class MobileContainer extends Component {
           <Menu.Item as='a'>All Wines</Menu.Item>
           <Menu.Item as='a'>Producers</Menu.Item>
           <Menu.Item as='a'>About Us</Menu.Item>
-          <Menu.Item as='a'>Log in</Menu.Item>
+          <Menu.Item as='a'>Register</Menu.Item>
           <Menu.Item as='a'>Members</Menu.Item>
         </Sidebar>
 
@@ -180,7 +136,7 @@ class MobileContainer extends Component {
                 </Menu.Item>
                 <Menu.Item position='right'>
                   <Button as='a' inverted>
-                    Log in
+                    Register
                   </Button>
                   <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
                     Members
@@ -188,7 +144,7 @@ class MobileContainer extends Component {
                 </Menu.Item>
               </Menu>
             </Container>
-            {/* <Navbar mobile /> */}
+            <Navbar mobile />
           </Segment>
 
           {children}
