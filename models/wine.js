e68a1b47dcc5dd29573f5df398287436b876b86b
FyperTheViper <b.w.byars@gmail.com>
2019-03-03 11:00:07 -0800

    changed schema to mongoose convention

--- models/wine.js
+++ models/wine.js
@@ -1,130 +1,48 @@
-// var mongoose = require('mongoose');
-// var Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-// var wineSchema = new Schema({
+var wineSchema = new Schema({
 
-//         wine_code: {
-//             type: String
-//         },
-//         country: {
-//             type: String
-//         },
-//         region: {
-//             type: DataTypes.STRING
-//         },
-//         subregion: {
-//             type: DataTypes.STRING
-//         },
-//         producer: {
-//             type: DataTypes.STRING
-//         },
-//         color: {
-//             type: DataTypes.STRING
-//         },
-//         wine_desc: {
-//             type: DataTypes.STRING
-//         },
-//         abv: {
-//             type: DataTypes.STRING
-//         },
-//         acidity: {
-//             type: DataTypes.STRING
-//         },
-//         redisual_sugar: {
-//             type: DataTypes.STRING
-//         },
-//         dosage: {
-//             type: DataTypes.STRING
-//         },
-//         years_blend: {
-//             type: DataTypes.STRING
-//         },
-//         bottle_date: {
-//             type: DataTypes.STRING
-//         },
-//         disgorgement: {
-//             type: DataTypes.STRING
-//         },
-//         closure: {
-//             type: DataTypes.STRING
-//         },
-//         annual_production: {
-//             type: DataTypes.STRING
-//         },
-//         varieties: {
-//             type: DataTypes.STRING
-//         },
-//         maceration: {
-//             type: DataTypes.STRING
-//         },
-//         pump_punch: {
-//             type: DataTypes.STRING
-//         },
-//         destem: {
-//             type: DataTypes.STRING
-//         },
-//         f_vessel: {
-//             type: DataTypes.STRING
-//         },
-//         f_duration: {
-//             type: DataTypes.STRING
-//         },
-//         aging_method: {
-//             type: DataTypes.STRING
-//         },
-//         aging_duration: {
-//             type: DataTypes.STRING
-//         },
-//         filtered: {
-//             type: DataTypes.STRING
-//         },
-//         fining: {
-//             type: DataTypes.STRING
-//         },
-//         yeast: {
-//             type: DataTypes.STRING
-//         },
-//         lees: {
-//             type: DataTypes.STRING
-//         },
-//         malo: {
-//             type: DataTypes.STRING
-//         },
-//         sulfur: {
-//             type: DataTypes.STRING
-//         },
-//         vinyard_name: {
-//             type: DataTypes.STRING
-//         },
-//         soil: {
-//             type: DataTypes.STRING
-//         },
-//         elevation: {
-//             type: DataTypes.STRING
-//         },
-//         aspect: {
-//             type: DataTypes.STRING
-//         },
-//         vine_age: {
-//             type: DataTypes.STRING
-//         },
-//         vine_yields: {
-//             type: DataTypes.STRING
-//         },
-//         farming_practices: {
-//             type: DataTypes.STRING
-//         },
-//         vine_training: {
-//             type: DataTypes.STRING
-//         },
-//         grape_picking: {
-//             type: DataTypes.STRING
-//         },
-//         notes: {
-//             type: String
-//         }
+        code: String,
+        country: String,
+        region: String,
+        subregion: String,
+        producer: String,
+        color: String,
+        wine: String,
+        alcohol_by_volume: String,
+        acidity: String,
+        redisual_sugar: String,
+        dosage: String,
+        years_in_blend: String,
+        bottling_date: String,
+        disgorgement_date: String,
+        closure: String,
+        annual_cases_produced: Number,
+        varieties: String,
+        maceration_notes: String,
+        pumpover: String,
+        destemming: String,
+        fermentation_vessel: String,
+        fermentation_duration: String,
+        aging_method: String,
+        aging_duration: String,
+        filtered: String,
+        fining_agents: String,
+        yeast: String,
+        lees_contact: String,
+        malolactic: String,
+        added_sulfur: String,
+        vinyard_name: String,
+        soil_types: String,
+        elevation: String,
+        vine_aspect: String,
+        vine_age: Number,
+        vine_yields: Number,
+        farming_practices: String,
+        vine_training_notes: String,
+        grape_picking: String,
+        notes: String
+    });
 
-
-//     });
-
-// module.exports = mongoose.model('Wine', wineSchema);
+module.exports = mongoose.model('Wine', wineSchema);
