95dca5aaaeacb612fb5d660bbbcc6cc72a86cced
Susye Weng-Reeder <weng-reeder@hotmail.com>
2019-03-10 18:16:16 -0700

    modal props

--- client/src/components/Team/Modal/Modal.js
+++ client/src/components/Team/Modal/Modal.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Button, Header, Image, Modal } from 'semantic-ui-react';
 
-const SalesTeamModal; = () => (
-  <Modal trigger={<Button>Show Modal</Button>}>
+const SalesTeamModal = () => (
+  <Modal trigger={<Button>Read Bio</Button>}>
     <Modal.Header>Select a Photo</Modal.Header>
     <Modal.Content image>
       <Image wrapped size='medium' src='/images/avatar/large/rachel.png' />
       <Modal.Description>
-        <Header>Profile Image</Header>
-        <p>teams.json</p>
-        <p>Is it okay to use this photo?</p>
+        <Header>Sales Team Profile</Header>
+        <p>bio information for each team member goes here</p>
+        <p>additional information for those who have two paragraphs</p>
       </Modal.Description>
     </Modal.Content>
   </Modal>
