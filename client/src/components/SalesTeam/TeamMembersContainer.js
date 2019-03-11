326501798ef495c131f0f54d9b3bdd5732e61144
Susye Weng-Reeder <weng-reeder@hotmail.com>
2019-03-11 13:29:12 -0700

    wrote algorithm for teamMember pairs

--- client/src/components/SalesTeam/TeamMembersContainer.js
+++ client/src/components/SalesTeam/TeamMembersContainer.js
@@ -1,16 +1,37 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
 import salesTeamMembers from "../../salesTeam.json";
+import TeamMemberCard from "./TeamMemberCard";
 
+const teamMemberCards = salesTeamMembers.map((member, i) => {
+    return <TeamMemberCard key={i} {...member} />
+});
+
+const pairs = [];
+for (let i=0; i < teamMemberCards.length; i+=2) {
+    let first = teamMemberCards[i];
+    let second = teamMemberCards[i+1];
+    pairs.push([first, second]);
+};
 
 
 function TeamMembersContainer(props) {
     return (
         <Container>
             <Grid>
-{}  salesTeamMembers && ()
-                  
-                
+            { pairs && (
+                pairs.map((pair, i) => {
+                    return (
+                        <Segment style={{ padding: '0em' }} vertical>
+                            <Grid celled='internally' columns='equal' stackable>
+                                <Grid.Row textAlign='center'>
+                                
+                                </Grid.Row>
+                            </Grid>
+                        </Segment>
+                    )
+                })
+            ) }
             </Grid>
         </Container>
 
