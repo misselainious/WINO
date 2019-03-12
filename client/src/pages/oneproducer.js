9e2fd371a3ed2287afb0665b4e740b08ec6cd848
michaelwhittemore <michaelwhittemore@gmail.com>
2019-03-11 21:32:32 -0700

    removed URL from one wine page

--- client/src/pages/oneproducer.js
+++ client/src/pages/oneproducer.js
@@ -22,7 +22,7 @@ class OneProducer extends Component {
 render() {
     const { producer } = this.state;
     delete producer._id
-    const producerObjKeys = Object.keys(producer);
+    const producerObjKeys = Object.keys(producer)
 
     return (
      <Grid>
