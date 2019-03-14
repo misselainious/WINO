581f69b2f41e5b8692fb85d4f08eec6cee05230b
Susye Weng-Reeder <weng-reeder@hotmail.com>
2019-03-14 13:07:47 -0700

    moved search bar to middle of allwines

--- client/src/components/SearchProducers/index.js
+++ client/src/components/SearchProducers/index.js
@@ -61,11 +61,11 @@ export default class SearchBar extends Component {
 
     return (
       <Grid style={{marginLeft: "auto", marginRight: "auto"}}>
-        <Grid.Row>
+        {/* <Grid.Row>
           {/* <Header>
             <h5 style={{marginLeft: 20}}>Search Our Producers</h5>
           </Header> */}
-        </Grid.Row>
+        </Grid.Row> */}
         <Grid.Column width={6}>
           <Search
             placeholder="Search producers..."
